9675e044bf35e406dd2758ea546e36343610d38b
arminsafie <arminsafaie92@gmail.com>
2024-01-30 18:21:54 +0330

    final

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+let displayValue = "";
+
+const updateDisplay = () => {
+  const display = document.getElementById("display");
+  display.value = displayValue;
+};
+
+const clearDisplay = () => {
+  displayValue = "";
+  updateDisplay();
+};
+
+const appendToDisplay = (value) => {
+  displayValue += value;
+  updateDisplay();
+};
+
+const calculateResult = () => {
+  try {
+    displayValue = eval(displayValue).toString();
+    updateDisplay();
+  } catch (error) {
+    displayValue = "Error";
+    updateDisplay();
+  }
+};
